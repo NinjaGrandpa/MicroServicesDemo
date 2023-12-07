a3621a466e688e72b80d2f25d307bc5460c2ebe3
NinjaGrandpa <34811547+NinjaGrandpa@users.noreply.github.com>
2023-12-07 11:46:01 +0100

    Implemented CRUD Operations

--- MicroServices/APIs/Questions/Questions.DataAccess/QuestionRepository.cs
+++ MicroServices/APIs/Questions/Questions.DataAccess/QuestionRepository.cs
@@ -1,36 +1,62 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Questions.DataAccess;
 
 public class QuestionRepository : IQuestionRepository
 {
+    private readonly IMongoCollection<Question> _collection;
+
+    public QuestionRepository()
+    {
+        var hostname = "localhost";
+        var databaseName = "QuestionDb";
+        var connectionString = $"mongodb://{hostname}:27017";
+
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase(databaseName);
+        _collection =
+            database.GetCollection<Question>(
+                "Questions",
+                new MongoCollectionSettings() { AssignIdOnInsert = true });
+    }
+
     public async Task<Question> GetByIdAsync(ObjectId id)
     {
-        throw new NotImplementedException();
+        var filter = Builders<Question>.Filter.Eq("_id", id);
+        return await _collection.Find(filter).FirstOrDefaultAsync();
     }
 
     public async Task<IEnumerable<Question>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        return await _collection.Find(_ => true).ToListAsync();
     }
 
     public async Task AddAsync(Question entity)
     {
-        throw new NotImplementedException();
+        await _collection.InsertOneAsync(entity);
     }
 
     public async Task UpdateAsync(Question entity)
     {
-        throw new NotImplementedException();
+        var filter = Builders<Question>.Filter.Eq("_id", entity.Id);
+        await _collection.ReplaceOneAsync(filter, entity);
     }
 
     public async Task DeleteAsync(ObjectId id)
     {
-        throw new NotImplementedException();
+        var filter = Builders<Question>.Filter.Eq("_id", id);
+        await _collection.DeleteOneAsync(filter);
     }
 
     public async Task<IEnumerable<Question>> GetForCategoryAsync(string category, int count, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var rnd = new Random();
+        var filter = Builders<Question>.Filter.Eq("Category", category);
+
+        var questionsWithCategory = await _collection.Find(filter).ToListAsync(cancellationToken);
+        var skip = rnd.Next(questionsWithCategory.Count - count);
+
+        return questionsWithCategory.Skip(skip).Take(count);
     }
 }
