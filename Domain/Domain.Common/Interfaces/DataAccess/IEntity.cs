215afe57782242345ed7bd9ed30bff726c2c8ac2
NinjaGrandpa <34811547+NinjaGrandpa@users.noreply.github.com>
2023-12-07 11:10:52 +0100

    Created interface IGenericRepository

--- Domain/Domain.Common/Interfaces/DataAccess/IEntity.cs
+++ Domain/Domain.Common/Interfaces/DataAccess/IEntity.cs
@@ -1,7 +1,9 @@
 namespace Domain.Common.Interfaces.DataAccess;
 
-// Interface som beskriver minsta möjliga som alla var typer ska ha
-public interface IEntity<T>
+// Interface som beskriver minsta möjliga som alla våra typer ska ha
+// Best practice att ange om den generiska typen är en in eller out typ
+
+public interface IEntity<out T>
 {
-    T Id { get; set; }
+    T Id { get; }
 }
---
+++ Domain/Domain.Common/Interfaces/DataAccess/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Common.Interfaces.DataAccess;
+
+// where T : class; kräver att den generiska typen kan initieras av 'new'
+// Generiskt för att fungera för flera olika typer
+
+public interface IGenericRepository<TEntity, TId> where TEntity : IEntity<TId>
+{
+    Task<TEntity> GetByIdAsync(TId id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TId id);
+}
